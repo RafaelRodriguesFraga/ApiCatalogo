e3430a69189a5bddcd8ce39a5535fe187ecd8eb0
Rafael Fraga <ti.rafaelfraga@gmail.com>
2020-03-26 16:16:15 -0300

    Criada as Models de Categoria e Produto

---
+++ ApiCatalogo/Models/Categoria.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiCatalogo.Models
+{
+    public class Categoria
+    {
+        public Categoria()
+        {
+            Produtos = new Collec   tion<Produto>();
+        }
+
+        public int Id { get; set; }
+        public int Nome { get; set; }
+        public int ImagemUrl { get; set; }
+
+        // Uma Categoria possui uma coleção de produtos
+        // (vários produtos)
+        public ICollection<Produto> Produtos;
+
+    }
+}
