c4a015a595b68ac53922be6faf3f6a5033a5c8b9
Rafael Fraga <ti.rafaelfraga@gmail.com>
2020-03-26 16:49:45 -0300

    Criadas as Migrations

---
+++ ApiCatalogo/Migrations/20200326194855_PopulaDB.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ApiCatalogo.Migrations
+{
+    public partial class PopulaDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
