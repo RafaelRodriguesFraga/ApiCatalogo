b68ba6127e8f6f11f3bd787e3ca0d6cdce1afda9
Rafael Fraga <ti.rafaelfraga@gmail.com>
2020-03-30 10:28:06 -0300

    Adiciona ProdutosController

---
+++ ApiCatalogo/Controllers/ProdutosController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using ApiCatalogo.Context;
+using ApiCatalogo.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiCatalogo.Controllers
+{
+    /// <summary>
+    /// Produtos
+    /// </summary>
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProdutosController : ControllerBase
+    {
+        private readonly AppDbContext _ctx;
+
+        public ProdutosController(AppDbContext ctx)
+        {
+            _ctx = ctx;
+        }
+
+        /// <summary>
+        /// Listar um Produto
+        /// </summary>
+        /// <returns>Uma lista de produtos</returns>
+        [HttpGet]
+        public ActionResult<IEnumerable<Produto>> ListarProdutos()
+        {
+            return _ctx.Produtos.AsNoTracking().ToList();
+        }
+
+        /// <summary>
+        /// Listar Produto por Id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>Produto</returns>
+        [HttpGet("{id}", Name = "ObterProduto")]
+        public ActionResult<Produto> ListarProdutoPorId(int id)
+        {
+            Produto produto = _ctx.Produtos.Where(p => p.Id == id).AsNoTracking().FirstOrDefault();
+            if (produto == null)
+                return NotFound();
+
+            return produto;
+        }
+
+        /// <summary>
+        /// Cadastrar Produto
+        /// </summary>
+        /// <param name="produto"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public ActionResult CadastrarProduto([FromBody] Produto produto)
+        {
+            _ctx.Produtos.Add(produto);
+            _ctx.SaveChanges();
+
+            return new CreatedAtRouteResult("ObterProduto", new { id = produto.Id }, produto);
+        }
+    }
+
+}
