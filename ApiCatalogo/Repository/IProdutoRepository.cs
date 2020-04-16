5ef471352c7e05abf629543632e5fc658c38d87e
Rafael Fraga <ti.rafaelfraga@gmail.com>
2020-04-16 11:20:56 -0300

    Cria Repositorios para Categoria e Produtos

---
+++ ApiCatalogo/Repository/IProdutoRepository.cs
@@ -0,0 +1,12 @@
+﻿using ApiCatalogo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiCatalogo.Repository
+{
+    interface IProdutoRepository : IRepository<Produto>
+    {
+        IEnumerable<Produto> GetProdutosPorPreco();     }
+}
