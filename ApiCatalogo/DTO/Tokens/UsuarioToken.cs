f3ce5cf2ae9f6c5e3d52a8556dfe4dff5f62d2a4
Rafael Fraga <ti.rafaelfraga@gmail.com>
2020-05-06 13:46:24 -0300

    Adiciona o DTO UsuarioToken

---
+++ ApiCatalogo/DTO/Tokens/UsuarioToken.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiCatalogo.DTO.Tokens
+{
+    public class UsuarioToken
+    {
+        public bool Autenticado { get; set; }
+        public DateTime DataExpiracao { get; set; }
+        public string Token { get; set; }
+        public string Mensagem { get; set; }
+    }
+}
