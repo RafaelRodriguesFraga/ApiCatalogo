0d0bde8e774763f1949a116b726bf0877f4099b6
Rafael Fraga <ti.rafaelfraga@gmail.com>
2020-05-06 11:31:15 -0300

    Adiciona UsuarioDTO

---
+++ ApiCatalogo/DTO/UsuarioDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiCatalogo.DTO
+{
+    public class UsuarioDTO
+    {
+        public string Email { get; set; }
+        public string Senha { get; set; }
+        public string ConfirmarSenha { get; set; }
+    }
+}
